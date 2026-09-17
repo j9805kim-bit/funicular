06fd6a31ed556e2f69f521c4fdc4c3b8fd17d742
j9805kim-bit <j9805kim@gamil.com>
2026-09-17 23:33:33 +0900

    Update app

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st 
 
+#git add main.py requirements.txt
+#git commit -m "Update app"
+#git push
+
 st.header("Hello World!!~" )
 
 age = st.slider("How old are you?", 0, 130, 25)
